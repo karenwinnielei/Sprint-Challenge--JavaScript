e8e80516645f2246e65af7fb8ea37c7e24a44ed4
Karen Lei <karenwinnielei@gmail.com>
2020-04-24 08:37:48 -0700

    complete closures task 1

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,13 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: nestedFunction can access the variable internal because variable values can be passed down/inherited from the parent function. Since myFunction is the parent of nestedFunction, nestedFunction inherits its variables.
 
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(){
+  
+}
